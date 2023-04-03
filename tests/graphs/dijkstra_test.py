9c2be6afbc6620597e51bb9dd619676bfec966ea
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-04-03 15:47:29 +0300

    Dijkstra shortest path

---
+++ tests/graphs/dijkstra_test.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import pytest
+
+from course.graphs.dijkstra import dijkstra_list_shortest_path
+from tests.graphs.graph_examples import graph_list_1, graph_list_2
+
+
+@pytest.mark.graph
+@pytest.mark.parametrize(
+    ("graph", "source", "needle", "expected_path"),
+    list(
+        zip(
+            (0, 4, 6, 2, 5),
+            (6, 2, 1, 5, 2),
+            (graph_list_1, graph_list_1, graph_list_2, graph_list_2, graph_list_2),
+            ([0, 1, 4, 5, 6], [4, 1, 2], [], [], [5, 2]),
+        ),
+    ),
+)
+def test_adjacency_list_dfs(graph: List[List[int]], source: int, needle: int, expected_path: List[int]) -> None:
+    # When
+    path = dijkstra_list_shortest_path(graph, source, needle)
+
+    # Then
+    assert path == expected_path
