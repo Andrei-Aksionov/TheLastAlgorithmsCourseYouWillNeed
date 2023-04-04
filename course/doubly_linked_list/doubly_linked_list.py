73c71e0b09972587511d1224dd7232ff2aebf856
Andrei.Aksionov <aksionau.andrei@gmail.com>
2023-04-04 16:49:49 +0300

    Map section of the course: LRU cache

--- course/doubly_linked_list/doubly_linked_list.py
+++ course/doubly_linked_list/doubly_linked_list.py
@@ -10,6 +10,11 @@ class Node:
 
 
 class LinkedList:
+    # -----      -----      -----      -----
+    # | A | <--> | B | <--> | C | <--> | D |
+    # -----      -----      -----      -----
+    #   ↑                                ↑
+    # head                              tail
     def __init__(self) -> None:
         self.length = 0
         self.head: Node = None
---
+++ course/maps/lru.py
@@ -0,0 +1,112 @@
+"""
+    Most Recently Used (MRU)                      Least Recently Used (LRU)
+      |                                           |
+      ⌄                                           ⌄
+    -----      -----      -----      -----      -----
+    | A | <--> | B | <--> | C | <--> | D | <--> | E |
+    -----      -----      -----      -----      -----
+      ^                    ^ ^         ^          ^
+      |                    | |         |          |
+     head               tail |         |         will be dropped with trim_cache()
+                             |         |         as it exceeds capacity
+                           length   capacity
+"""
+
+from typing import Optional
+
+
+class Node:
+    def __init__(self, value: int, next: Optional["Node"] = None, prev: Optional["Node"] = None) -> None:  # noqa: A002
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+
+class LRU:
+    def __init__(self, capacity: int = 10) -> None:
+        self.length = 0
+        self.capacity = capacity
+        self.head = self.tail = None
+        self.lookup = {}
+        self.reverse_lookup = {}
+
+    def update(self, key: str, value: int) -> None:
+        """Update node's value that is accessed by the key if it exists, create new if doesn't."""
+
+        node = self.lookup.get(key)
+        # if node doesn't exist - create one with the value and put it in the beginning
+        if node is None:
+            node = Node(value)
+            self._prepend(node)
+            # as new node is added in case the length exceeds capacity we need to trim cache
+            self._trim_cache()
+            self.lookup[key] = node
+            self.reverse_lookup[node] = key
+        else:
+            # if node exists - just put it in the beginning
+            self._detach(node)
+            self._prepend(node)
+            node.value = value
+
+    def get(self, key: int) -> Optional[Node]:
+        """Return node's value if it can be accessed by the key, in other case - return None."""
+        # check cache for existence
+        node = self.lookup.get(key)
+        if node is None:
+            return None
+
+        # update the value we found and move it to the front
+        self._detach(node)
+        self._prepend(node)
+
+        # return the value we found or None if not exist
+        return node.value
+
+    def _detach(self, node: Node) -> None:
+        # if there is a node in front
+        if node.prev:
+            node.prev.next = node.next
+        # if there is a node behind
+        if node.next:
+            node.next.prev = node.prev
+
+        # update head/tail if needed
+        if node == self.head:
+            self.head = self.head.next
+        if node == self.tail:
+            self.tail = self.tail.prev
+
+        # unlink the node
+        node.prev = node.next = None
+
+        # don't forget to update the length
+        self.length -= 1
+
+    def _prepend(self, node: Node) -> None:
+        self.length += 1
+
+        # if it's just an empty cache
+        if not (self.head and self.tail):
+            self.head = self.tail = node
+            return
+
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+
+    def _trim_cache(self) -> None:
+        """If the length of the cache exceeds it's capacity remove the last node and cleanup lookup tables."""
+        if self.length <= self.capacity:
+            return
+
+        node = self.tail
+        # unlink the last node
+        self._detach(node)
+
+        # cleanup lookup tables
+        key = self.reverse_lookup.get(node)
+        self.lookup.pop(key)
+        self.reverse_lookup.pop(node)
